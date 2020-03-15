b56033ec5a3e77374f4ae91acf34d69a540445b5
Nickolay <nickkulman@gmail.com>
2020-03-15 16:25:47 +0300

    JQuery

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.main_btna, .main_btn').on('click', function() {
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
+    });
+
+    $('a[href="#sheldure"]').on('click', function() {
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
+    });
+
+    $('.close').on('click', function() {
+        $('.overlay').fadeOut(1000);
+        $('.modal').slideUp(1000);
+    });
+  });
